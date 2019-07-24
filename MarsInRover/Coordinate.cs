aedeb4a1b665a2aa4c4e4b42c88f409093479c93
Shubham Gogawale <sgogawale@affinionds.com>
2019-07-24 23:18:18 +0530

    Added Support for coordinates

---
+++ MarsInRover/Coordinate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsInRover
+{
+    public class Coordinate
+    {
+        public object x { get; set; }
+        public int y { get; set; }
+    }
+}
---
+++ RoverInMars.Test/CordinateFixture.cs
@@ -0,0 +1,19 @@
+using System;
+using MarsInRover;
+using FluentAssertions;
+
+using Xunit;
+
+namespace RoverInMars.Test
+{
+    public class CordinateFixture
+    {
+        [Fact]
+        public void Can_store_coordinate()
+        {
+            Coordinate corr = new Coordinate() { x = 15, y = 3 };
+            corr.x.Should().Be(15);
+            corr.y.Should().Be(3);
+        }
+    }
+}
